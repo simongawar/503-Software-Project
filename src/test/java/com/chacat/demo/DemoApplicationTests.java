03f209d19e48a80108ff26c68b3970e476f85fb2
simongawar <g.dakthak@gmail.com>
2024-11-27 01:38:37 +0200

    Add files via upload

---
+++ src/test/java/com/chacat/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
